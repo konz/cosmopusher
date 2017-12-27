b5341f7389b011b9fbc09e60ff45c65d52b90935
Konrad Hosemann <konrad.hosemann@gmail.com>
2017-12-27 16:45:46 +0100

    just print, remove playing around with delays

--- src/main/python/cosmopusher/print_pusher.py
+++ src/main/python/cosmopusher/print_pusher.py
@@ -1,32 +1,4 @@
-from random import randint
+class PrintPusher:
 
-import gevent
-from gevent import Greenlet
-
-
-class PrintLine(Greenlet):
-
-    def __init__(self, payload):
-        Greenlet.__init__(self)
-        self.payload = payload
-        self.delayed = 0
-
-    def _run(self):
-        try:
-            self.send()
-        except Exception:
-            gevent.sleep(1)
-            self.delayed += 1
-            self._run()
-
-    def send(self):
-        if randint(0, 1) == 0:
-            raise Exception
-        else:
-            print("DELAYED: {}: {}".format(self.delayed, self.payload))
-
-
-class PrintPusher(Greenlet):
-
-    def push(self, _, payload):
-        PrintLine.spawn(payload)
+    def push(self, event_type, payload):
+        print("{}: {}".format(event_type, payload))
